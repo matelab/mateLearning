ed198c1f79fe0be5a72de5322573a7264976993b
Juan P. Romano <programming@jpromano.net>
2021-04-26 11:26:36 -0300

    key sounds script

--- 01_Javascript/jromano/01_DrumKit/js/script.js
+++ 01_Javascript/jromano/01_DrumKit/js/script.js
@@ -1,4 +1,4 @@
-window.addEventListener('keydown', function(e){
+function playSound(e){
     const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
     const key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
     // stop the function from running all together
@@ -7,12 +7,16 @@ window.addEventListener('keydown', function(e){
     audio.currentTime = 0;
     audio.play();
     key.classList.add('playing');
-});
-
+}
+    
 function removeTransition(e){
     // skip it if it's not a transform
     if(e.propertyName !== 'transform') return;
+    this.classList.remove('playing');
 }
 
 const keys = document.querySelectorAll('.key');
 keys.forEach(key => key.addEventListener('transitionend', removeTransition));
+
+// Event listening to the index window
+window.addEventListener('keydown', playSound);
