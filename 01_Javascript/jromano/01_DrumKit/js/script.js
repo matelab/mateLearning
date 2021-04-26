df72d876dd98d19aa8f615fb5fcf872d0a9db7d3
Juan P. Romano <programming@jpromano.net>
2021-04-26 11:08:29 -0300

    key sounds script

--- 01_Javascript/jromano/01_DrumKit/js/script.js
+++ 01_Javascript/jromano/01_DrumKit/js/script.js
@@ -3,8 +3,8 @@ window.addEventListener('keydown', function(e){
     const key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
     // stop the function from running all together
     if(!audio) return;
+    // Rewind to the start
     audio.currentTime = 0;
     audio.play();
-    key.addClass('playing');
     key.classList.add('playing');
-})
+});
