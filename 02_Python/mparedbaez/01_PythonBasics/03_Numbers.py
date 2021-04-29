b7fc6f4277578244ac48182124f1f05a8f53d0c0
baubyte <paredbaez.martin@gmail.com>
2021-04-29 12:24:54 -0300

    01_PythonBasics

---
+++ 02_Python/mparedbaez/01_PythonBasics/03_Numbers.py
@@ -0,0 +1,22 @@
+
+
+print(2)
+print(2.0987)
+print(3 * 4.5)
+print(3 * 4 + 5)
+print(3 * (4 + 5))
+#Modulo
+print(10 % 3)
+
+myNum = 5
+print(str(myNum)+ " my numbers")
+#print(myNum + " my numbers")
+numAbs = -5
+print(abs(numAbs))
+#potencia
+print(pow(4,6))
+#max
+print(max(4,6))
+#round
+print(round(3.2))
+print(round(3.7))
