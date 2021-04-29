117e1a8117920bcc7c7573330580c74af51d2481
juanPabloCesarini <juanpcesarini@hotmail.com>
2021-04-29 12:08:58 -0300

    numbers function

---
+++ 02_Python/jcesarini/numbers.py
@@ -0,0 +1,11 @@
+print(3* (4+5)) # Fist sum second multimplication
+
+print(10%3) 
+
+my_num = 5
+
+print(str(my_num) + " my favorite number")
+my_num = -5
+print(abs(my_num))
+print(pow(3,2))
+print(round(3.57))
