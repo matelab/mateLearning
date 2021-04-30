2fa3cb4d5dba2a72183d3f0460646cba63a783ae
Juan P. Romano <programming@jpromano.net>
2021-04-30 11:03:16 -0300

    Python lists

--- 02_Python/jromano/01_PythonBasics/list.py
+++ 02_Python/jromano/01_PythonBasics/list.py
@@ -20,3 +20,24 @@ print(friends)
 # Insert
 friends.insert(1, "Kelly")
 print(friends)
+
+# Figure out the index of an element in the list
+print(friends.index("Jim"))
+
+# Count values in a list
+print(friends.count("Jim"))
+
+# Sort the list
+print(friends.sort())
+
+# Remove
+friends.remove("Jim")
+print(friends)
+
+# Pop
+friends.pop()
+print(friends)
+
+# Clear list
+friends.clear()
+print(friends)
