7bf5fc1c6aadc88b362bc82cfa97851ab85d1732
baubyte <paredbaez.martin@gmail.com>
2021-04-30 11:40:22 -0300

    07 MadLibs

---
+++ 02_Python/mparedbaez/01_PythonBasics/07.MadLibs.py
@@ -0,0 +1,8 @@
+
+color = input("Enter a Color: ")
+pluralNoun = input("Enter a Plural Noun: ")
+celebrity = input("Enter a Celebrity: ")
+
+print(f"Rose are "+color)
+print(pluralNoun + "are blue")
+print(f"I Love "+celebrity)
