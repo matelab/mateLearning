ee2314c799712395ef17de2d54de7d64362f18ce
Juan P. Romano <programming@jpromano.net>
2021-04-30 10:40:48 -0300

    Python lists

--- 02_Python/jromano/01_PythonBasics/list.py
+++ 02_Python/jromano/01_PythonBasics/list.py
@@ -1,5 +1,9 @@
 # This is a list
-friends = ["Kevin", "Karen", "Jim"]
+friends = ["Kevin", "Karen", "Jim", "Oscar", "Toby"]
+friends[1] = "Micheal Scott"
 
 # How to access element in a list?
 print(friends[0])
+
+# Many elements
+print(friends[1:3])
