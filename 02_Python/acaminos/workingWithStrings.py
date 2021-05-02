816a0cd79654a1c83345b06164e9764ba268ea73
ACaminos <caminosariel@gmail.com>
2021-05-02 12:58:16 -0300

    finish

---
+++ 02_Python/acaminos/workingWithStrings.py
@@ -0,0 +1,10 @@
+phrase = "Giraffe Academy"
+print("\n", phrase.upper().isupper(),"\n")
+
+print((len(phrase)),"\n")
+
+print(phrase[3], "\n")
+
+print(phrase.index("G"), "\n")
+
+print(phrase.replace("Giraffe", "Elephant"),"\n")
