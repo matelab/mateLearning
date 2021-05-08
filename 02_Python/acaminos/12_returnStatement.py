b64c317c7e701a7bd98d9e2db4d3a42ec467fb52
ACaminos <caminosariel@gmail.com>
2021-05-07 23:46:05 -0300

    finish

---
+++ 02_Python/acaminos/12_returnStatement.py
@@ -0,0 +1,9 @@
+def cube(num):
+    return num*num*num
+    print('blabla') #esto no lo va a leer, ya que hace un break en el retunr
+
+print(cube(3))
+
+# or ... 
+# result = cube(4)
+# print(result)
