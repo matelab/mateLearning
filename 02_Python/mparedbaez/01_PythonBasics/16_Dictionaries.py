453d05fabf60b9adcbf2490d3a71cb3e63747366
baubyte <paredbaez.martin@gmail.com>
2021-05-13 12:53:04 -0300

    Dictionaries

---
+++ 02_Python/mparedbaez/01_PythonBasics/16_Dictionaries.py
@@ -0,0 +1,19 @@
+
+monthConversions = {
+    "Jan" : "January",
+    "Feb" : "February",
+    "Mar" : "March",
+    "Apr" : "April",
+    "May" : "May",
+    "Jun" : "June",
+    "Jul" : "July",
+    "Aug" : "August",
+    "Sep" : "September",
+    "Oct" : "October",
+    "Nov" : "November",
+    "Dec" : "December",
+}
+print(monthConversions['Mar'])
+print(monthConversions.get('Dec'))
+print(monthConversions.get('Luv'))
+print(monthConversions.get('Luv', 'Not a Valid Key'))
