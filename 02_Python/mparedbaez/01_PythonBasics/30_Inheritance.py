e1059324982f2d26b6c225c077c9f3e11d6fcc0b
baubyte <paredbaez.martin@gmail.com>
2021-05-27 12:34:51 -0300

    Terminado

---
+++ 02_Python/mparedbaez/01_PythonBasics/30_Inheritance.py
@@ -0,0 +1,13 @@
+#Herencia
+
+from Chef import Chef
+from ChineseChef import ChineseChef
+
+chef = Chef()
+chef.makeChicken()
+chef.makeSpecialDish()
+
+chineseChef = ChineseChef()
+chineseChef.makeSpecialDish()
+chineseChef.makeFriedRice()
+chineseChef.makeChicken()
